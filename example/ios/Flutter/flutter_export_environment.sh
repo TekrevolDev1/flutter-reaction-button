14af5e85fdbefc68b9aac488b3eddc881388580e
Aamir Saleem <aamir@tekrevol.com>
2022-05-25 15:44:55 +0500

    Adjusted offset for doodl

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/rickeylee/Desktop/dev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/rickeylee/Desktop/flutter-reaction-button/example"
+export "FLUTTER_ROOT=/Users/macbook/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/macbook/Documents/FlutterProjects/flutter-reaction-button/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
